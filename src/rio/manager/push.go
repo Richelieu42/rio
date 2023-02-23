046fffa6d2174dc7aefea9843b84687fc41fb8bb
Richelieu <richelieu042@gmail.com>
2023-02-23 10:21:02 +0800

    init

--- src/rio/manager/push.go
+++ src/rio/manager/push.go
@@ -5,7 +5,7 @@ import (
 	"github.com/richelieu42/go-scales/src/core/sliceKit"
 )
 
-func PushToBsId(messageType int, bsid string, data []byte) {
+func PushToBsId(messageType int, data []byte, bsid string) {
 	rwLock.RLock()
 	defer rwLock.RUnlock()
 
@@ -17,16 +17,41 @@ func PushToBsId(messageType int, bsid string, data []byte) {
 	_ = channel.PushMessage(messageType, data)
 }
 
-func PushToGroup(messageType int, group string, data []byte, exceptBsIds []string) {
+func PushToUser(messageType int, data []byte, user string, exceptBsIds ...string) {
 	rwLock.RLock()
 	defer rwLock.RUnlock()
 
-	s := mapKit.Get(groupMap, group)
+	channels := mapKit.Get(userMap, user)
 
-	for _, channel := range s {
+	for _, channel := range channels {
 		if sliceKit.Contains(exceptBsIds, channel.GetBsId()) {
 			continue
 		}
+		// TODO: 输出推送失败的error
+		_ = channel.PushMessage(messageType, data)
+	}
+}
+
+func PushToGroup(messageType int, data []byte, group string, exceptBsIds ...string) {
+	rwLock.RLock()
+	defer rwLock.RUnlock()
+
+	channels := mapKit.Get(groupMap, group)
+
+	for _, channel := range channels {
+		if sliceKit.Contains(exceptBsIds, channel.GetBsId()) {
+			continue
+		}
+		// TODO: 输出推送失败的error
+		_ = channel.PushMessage(messageType, data)
+	}
+}
+
+func PushToAll(messageType int, data []byte) {
+	rwLock.RLock()
+	defer rwLock.RUnlock()
+
+	for _, channel := range allMap {
 		// TODO: 输出推送失败的error
 		_ = channel.PushMessage(messageType, data)
 	}
