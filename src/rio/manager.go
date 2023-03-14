75dced2fcf1c5db249956f67506c848cbf909226
Richelieu <richelieu042@gmail.com>
2023-03-14 11:04:47 +0800

    init

--- src/rio/manager.go
+++ src/rio/manager.go
@@ -3,6 +3,7 @@ package rio
 import (
 	mapset "github.com/deckarep/golang-set/v2"
 	"github.com/richelieu42/go-scales/src/core/mapKit"
+	"github.com/richelieu42/go-scales/src/core/setKit"
 	"github.com/richelieu42/go-scales/src/core/strKit"
 	"sync"
 )
@@ -61,4 +62,37 @@ func RemoveChannel(channel *Channel) (flag bool) {
 	}
 
 	return flag
+}
+
+func bindData(channel *Channel, bsId, user, group string) {
+	rwLock.Lock()
+	defer rwLock.Unlock()
+
+	if strKit.IsNotEmpty(bsId) {
+		channel.SetBsId(bsId)
+
+		mapKit.Set(bsIdMap, bsId, channel)
+	}
+
+	if strKit.IsNotEmpty(user) {
+		channel.SetUser(user)
+
+		set := mapKit.Get(userMap, user)
+		if set == nil {
+			set = setKit.NewSet[*Channel](false)
+			mapKit.Set(userMap, user, set)
+		}
+		set.Add(channel)
+	}
+
+	if strKit.IsNotEmpty(group) {
+		channel.SetGroup(group)
+
+		set := mapKit.Get(groupMap, group)
+		if set == nil {
+			set = setKit.NewSet[*Channel](false)
+			mapKit.Set(groupMap, group, set)
+		}
+		set.Add(channel)
+	}
 }
